67e063c1917ceb6c251c6b0469029b40d364f8a0
Rafael Dutra <rafaeldut89@gmail.com>
2018-01-31 17:47:24 -0200

    first commit

---
+++ UnitTestProject1/CalculationManager.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace UnitTestProject1
+{
+    public class CalculationManager
+    {
+        public double Add(object initNumber, object additional)
+        {
+            double baseNumber = Convert.ToDouble(initNumber);
+            double addingNumber = Convert.ToDouble(additional);
+            return baseNumber + addingNumber;
+        }
+    }
+
+    public interface ICalculator
+    {
+        double Add(double firstNumber, double lastNumber);
+    }
+
+    public class Mathematics
+    {
+        private ICalculator calculator;
+        public Mathematics(ICalculator calc)
+        {
+            calculator = calc;
+        }
+        public double AddNumbers()
+        {
+            return calculator.Add(1, 1);
+        }
+    }
+}
---
+++ UnitTestProject1/UnitTest1.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace UnitTestProject1
+{
+    [TestClass]
+    public class UnitTest1
+    {
+        [TestMethod]
+        public void Add_Test()
+        {
+            CalculationManager manager = new CalculationManager();
+            Assert.IsTrue(manager.Add(2, 2).Equals(4), "2 + 2 = 4");
+            Assert.IsTrue(manager.Add(2, 2.5).Equals(4.5), "2 + 2.5 = 4.5");
+            try { manager.Add(DateTime.Now, 2.5); }
+            catch (Exception ex)
+            {
+                Assert.IsInstanceOfType(ex,
+                    typeof(InvalidCastException), "Today + 2 = oops");
+            }
+        }
+
+        [TestMethod]
+        public void Index_Test()
+        {
+            //CalculationController controller = new CalculationController();
+            //Assert.IsInstanceOfType(controller.Index(), typeof(ExpectedViewResult));
+        }
+
+        [TestMethod]
+        public void TestAdd()
+        {
+            //// Create the fake calculator:
+            //ICalculator calculator = new Calculator.Fakes.StubICalculator()
+            //{
+            //    // Define each method:
+            //    Add = (a, b) => { return 25; }
+            //};
+            //// In the completed application, item would be a real one:
+            //var item = new Mathematics(calculator);
+            //   double added = item.AddNumbers();
+            //Assert.AreEqual(25, added);
+        }
+    }
+}
